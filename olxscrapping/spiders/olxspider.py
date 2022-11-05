ea82e8c0a2e2cfe46b0baed9911a49052276b310
unknown <rishikt8456@gmail.com>
2022-11-05 13:11:12 +0530

    third comit

--- olxscrapping/spiders/olxspider.py
+++ olxscrapping/spiders/olxspider.py
@@ -5,8 +5,20 @@ class olxspider(scrapy.Spider):
     start_urls = ['https://www.olx.in/kozhikode_g4058877/for-rent-houses-apartments_c1723']
 
     def parse(self,response):
-        for product in response.css("li._1DNjI"):
-            yield{
-                'name': product.css("span.YBbhy::text").get(),
-                'price':product.css("span._2Ks63::text").get().replace('₹',''),
-            }
+        for products in response.css("li._1DNjI"):
+            try:
+                yield{
+                    'name': products.css('span.YBbhy::text').get(),
+                    'price':products.css('span._2Ks63::text').get().replace('₹',''),
+                }
+            except:
+                yield{
+                    'name': products.css('span.YBbhy::text').get(),
+                    'price':'sold out',
+
+                 
+                }
+        next_page = response.css('button.rui-39-wj.rui-3evoE.rui-1JPTg')
+        if next_page is not None:
+            yield response.follow(next_page,callback=self.parse)
+        
