f5401143504c38143e8103d04eb5ea99ca1a9577
unknown <rishikt8456@gmail.com>
2022-11-06 06:51:24 +0530

    fourth commit

--- olxscrapping/spiders/olxspider.py
+++ olxscrapping/spiders/olxspider.py
@@ -1,24 +1,26 @@
 import scrapy
+# from urllib.parse import urljoin
 
 class olxspider(scrapy.Spider):
     name ='olx'
     start_urls = ['https://www.olx.in/kozhikode_g4058877/for-rent-houses-apartments_c1723']
 
     def parse(self,response):
+        
         for products in response.css("li._1DNjI"):
             try:
                 yield{
-                    'name': products.css('span.YBbhy::text').get(),
-                    'price':products.css('span._2Ks63::text').get().replace('₹',''),
+                    'link':products.css("li._1DNjI a").xpath("@href").extract()
                 }
             except:
                 yield{
-                    'name': products.css('span.YBbhy::text').get(),
-                    'price':'sold out',
-
-                 
+                    'link': 'sold out',
                 }
-        next_page = response.css('button.rui-39-wj.rui-3evoE.rui-1JPTg')
-        if next_page is not None:
-            yield response.follow(next_page,callback=self.parse)
+        # for p in products:
+        #     url=urljoin(response.url,p)
+        #     yield scrapy.request(url,callback=self.parse_product)
+
+        # next_page = response.css('button.rui-39-wj.rui-3evoE.rui-1JPTg')
+        # if next_page is not None:
+        #     yield response.follow(next_page,callback=self.parse)
         
