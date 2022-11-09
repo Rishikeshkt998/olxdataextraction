39434516b9e63e2e8deba281aee04b0045e6a65d
unknown <rishikt8456@gmail.com>
2022-11-09 15:51:13 +0530

    sixth commit

--- olxscrapping/spiders/olxspider.py
+++ olxscrapping/spiders/olxspider.py
@@ -1,13 +1,14 @@
 import scrapy
 from urllib.parse import urljoin
+import json
 
 class olxspider(scrapy.Spider):
     name ='olx'
     start_urls = ['https://www.olx.in/kozhikode_g4058877/for-rent-houses-apartments_c1723']
-    # headers={'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 Edg/107.0.1418.35'}
+    
+    
+    
     def parse(self,response):
-        
-        
         urls= response.css("li._1DNjI a").xpath("@href").extract()
         for url in urls:
             url=response.urljoin(url)
@@ -18,7 +19,10 @@ class olxspider(scrapy.Spider):
             'property_name':response.xpath("//h1[@class='_1hJph']/text()").extract(),
             'property_id':response.xpath("//div[contains(@class, '_1-oS0')]/strong/text()").extract()[2],
             'breadcrumbs':response.xpath('//ol[@class="rui-10Yqz"]/li/a/text()').extract(),
-            'price':response.xpath("//span[@class='T8y-z']/text()").extract(),
+            'price':{
+                'amount':response.xpath("//span[@class='T8y-z']/text()").extract()[0][2:],
+                'currency':response.xpath("//span[@class='T8y-z']/text()").extract()[0][0]
+            },
             'image_url':response.xpath("//div[@class='_23Jeb']/figure/img/@src").extract_first(),
             'description':response.xpath("//div[@data-aut-id='itemDescriptionContent']/p/text()").extract(),
             'seller_name':response.css('div.eHFQs::text').extract(),
@@ -29,9 +33,7 @@ class olxspider(scrapy.Spider):
 
 
         }
-    # def start_requests(self):
-    #     for page in range(0,3):
-    #       next_page=self.start_urls+str(page)
-    #     yield scrapy.Request(url=next_page,headers=self.headers,callback=self.parse)
+    
+        
 
 
